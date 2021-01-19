473d66a41197730d7f7315679b03f30b19c92b82
TalShalmon <talshalmon@gmail.com>
2021-01-19 15:26:30 +0200

    C# Project with System class

---
+++ MyCloud/Stratup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCloud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
